340fdd1df14a188a0d80910772c81057e912833f
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-06-04 22:37:27 -0500

    feat: :rocket: Octavo ejercicio
    Buscar un planeta del sistema solar para una mision espacial, en ese ejercicio utilizamos el metodo: arra_rank para elegir un valor del array ya dispuesto aleatoremente.

--- index.php
+++ index.php
@@ -8,47 +8,19 @@
 </head>
 <body>
    
-    <h1>Buscador de Naves</h1>
-    <h3>Ingrese la nave a buscar seguido de oprimir el boton Buscar</h3>
-   
+    <h1>Buscador de planetas aleatorio para misiones espaciales</h1>
     <form method="POST">
-  
-        <select class="form-select" aria-label="Default select example" name="tipo_nave">
-            <option selected>TIPO DE NAVE</option>
-            <option value="nave tipo A">Nave tipo A</option>
-            <option value="nave tipo B">Nave tipo B</option>
-            <option value="nave tipo C">Nave tipo C</option>
-            <option value="nave tipo D">Nave tipo D</option>
-            <option value="nave tipo E">Nave tipo E</option> 
-            <option value="nave tipo F">Nave tipo F</option> 
-            <option value="nave tipo G">Nave tipo G</option> 
-            <option value="nave tipo H">Nave tipo H</option> 
-            <option value="nave tipo I">Nave tipo I</option> 
-        </select>
-        <input type="submit" value="buscar">
-     </form>
-</body>
-</html>
+    <input type="submit" value="buscar" name="buscar" >
+    </form>
+ <?php
 
+$sistema_solar=["Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno"];
 
-<?php
+$claves_aleatorias = array_rand($sistema_solar,2);
 
-$tipos_de_naves=["nave tipo A", "nave tipo C", "nave tipo E", "nave tipo F", "nave tipo H"];
-
-session_start(); 
-
-if (isset($_POST['tipo_nave'])) { 
-   
-    $nave = $_POST['tipo_nave']; 
-
-if(in_array($nave,$tipos_de_naves)){
-    echo "<br>", $nave, " Sí existe en la flota";
+if (isset($_POST['buscar'])) { 
+echo"<br>el planeta elegido para la misión espacial es: ", $sistema_solar[$claves_aleatorias[0]] . "\n";
 }
-    else{
-        echo "<br>", $nave," No existe en la flota";
-    }
-}
-
 ?>
 
 
