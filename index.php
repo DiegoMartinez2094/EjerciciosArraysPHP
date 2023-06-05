5e18815ecd188353440cf47586db33969d5f6c3b
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-06-04 23:24:46 -0500

    feat: :rocket: Duodécimo ejercicio
    en este ejercicio agregamos nombres de lunas de jupiter a la lista, para eso utilizamos el metodo array_push(): el cual nos agrega valores al final de una rray modificandolo

--- index.php
+++ index.php
@@ -1,29 +1,18 @@
 
  <?php
 
-$sistemas=["mercurio", "tierra", "venus", "Marte", "urano","TrES-2b"];
-echo " <h2>planetas del sistema 1</h2>";
-foreach ($sistemas as $sistema) {;
-    echo '<p>'. $sistema. '</p>';
+$lunasJupiters=["europa", "ganimedes", "ío", "calisto", "amaltea","adrastea"];
+echo " <h2>Algunas lunas de Jupiter:</h2>";
+foreach ($lunasJupiters as $lunasJupiter) {;
+    echo '<p>'. $lunasJupiter. '</p>';
 }
 
-$planetas=["tierra", "venus", "Marte", "kepler", "J1407b","TrES-2b"];
-echo " <h2>planetas del sistema 2</h2>";
-foreach ($planetas as $planeta) {;
-    echo '<p>'. $planeta. '</p>';
+array_push($lunasJupiters, "tebe", "carmé", "autónoe");
+echo " <h2>lista más grande de las lunas de Jupiter:</h2>";
+foreach ($lunasJupiters as $lunasJupiter) {;
+    echo '<p>'. $lunasJupiter. '</p>';
 }
 
-echo " <h2>planetas unicos en el sistema solar 1:</h2>";
-$results = array_diff($sistemas, $planetas);
-foreach ($results as $result) {;
-    echo '<p>'. $result. '</p>';
-}
-
-echo " <h2>planetas unicos en el sistema solar 2:</h2>";
-$results = array_diff($planetas,$sistemas);
-foreach ($results as $result) {;
-    echo '<p>'. $result. '</p>';
-}
 
 ?>
 
