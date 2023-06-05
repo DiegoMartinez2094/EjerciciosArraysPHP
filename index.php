fef20b3d51bfe9277d8d168a51be21a9fc0caaf4
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-06-04 21:25:27 -0500

    feat: :rocket: Cuarto ejercicio
    ejercicio sobre la gravedad de los planetas en funcion de la tierra, aplicamos la funcion array_walk()

--- index.php
+++ index.php
@@ -1,57 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>planetas habitables</title>
-</head>
-
-<body>
-
-    <h3>seleccionador de planetas habitables </h3>
-    <h5>Ingrese los planetas y su caracteristica habitable o no habitable</h5>
-
-    <form method="POST">
-        <input type="text" name="planeta" placeholder="nombre planeta">
-
-        <select class="form-select" aria-label="Default select example" name="habitable">
-            <option selected>es habitable?</option>
-            <option value="habitable">habitable</option>
-            <option value="no habitable">no habitable</option>
-        </select>
-        <input type="submit" value="enviar">
-        <input type="submit" name="mostrar_habitables" value="mostrar habitables">
-
-        </ </form>
-
-</body>
-
-</ </html>
 
 <?php
 
-session_start(); //permite almacenar variables de una session en el servidor
+$gravedadEnFuncion =["Mercurio"=>37.8,"Venus"=> 90.5,"Marte"=> 37.86,"Jupiter"=> 252.96,"Saturno"=> 106.54,"Urano"=> 90.52,"Neptuno"=> 113.8];
 
-if (isset($_POST['planeta']) && isset($_POST['habitable'])) { // verifica si las variablesplanetay habitablese envía
-    $planeta = $_POST['planeta']; //almacenamos en variables
-    $habitable = $_POST['habitable']; //los datos enviados mediante post
+echo"gravedad de Mercurio es: ",$gravedadEnFuncion["Mercurio"], " veces a la de la tierra <br>";
+echo"gravedad de Venus  es: ",$gravedadEnFuncion["Venus"], " veces a la de la tierra <br>";
+echo"gravedad de Marte es: ",$gravedadEnFuncion["Marte"], " veces a la de la tierra <br>";
+echo"gravedad de Jupiter es:  ",$gravedadEnFuncion["Jupiter"], " veces a la de la tierra <br>";
+echo"gravedad de saturno es: ",$gravedadEnFuncion["Saturno"], " veces a la de la tierra <br>";
+echo"gravedad de Urano es: ",$gravedadEnFuncion["Urano"], " veces a la de la tierra <br>";
+echo"gravedad de Neptuno es: ",$gravedadEnFuncion["Neptuno"], " veces a la de la tierra <br> <br>";
 
-    $_SESSION['sistema'][$habitable][] = $planeta;
-
-    //agrega el valor de $planeta al array `$_SESSION['sistema']$_SESSION['sistema']
-    //utilizando $habitablecomo clave. Esto significa que se está registrando el planeta en una categoría específica según su habitabilidad.
+function gravedadReal ($a){
+    echo number_format($a*0.098, 2) ," gravedad real del planeta <br>";
 }
-
-if (isset($_POST['mostrar_habitables'])) { 
-    // verifica si la variable mostrar_habitables se envió a través de una solicitud POST.
-    // Esto indica que el usuario desea mostrar los planetas habitables registrados.
-    if (isset($_SESSION['sistema']['habitable'])) { //verifica si hay registro de algun planeta habitable
-        echo " <br> <br>Planetas habitables: <br> ".implode(", ", $_SESSION['sistema']['habitable']); //implode se utiliza para convertir array a texto
-    } else {
-        echo "No hay planetas habitables registrados.";
-    }
-}
+array_walk($gravedadEnFuncion, 'gravedadReal');
 ?>
